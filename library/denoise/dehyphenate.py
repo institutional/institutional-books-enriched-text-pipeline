03409a4a0c71f071ffc764c6bb3cd203b20b1ab4
David Lowry-Duda <david@lowryduda.com>
2026-03-26 11:58:12 -0400

    Improve ngram unknown language consistency

--- library/denoise/dehyphenate.py
+++ library/denoise/dehyphenate.py
@@ -93,6 +93,7 @@ def load_ngram_scorer(lang: str, model_dir: Path, cache: dict[str, NGramScorer])
         )
         logger.warning("Defaulting to English for dehyphenation")
         stats_path = model_dir / "eng_ngram.json.gz"
+        lang = "eng"
     stats = load_ngram_stats(stats_path)
     cache[lang] = NGramScorer(stats)
     return cache[lang]
